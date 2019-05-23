5ab98b4804f096ca0f603a0c43fab4faf1157269
Irishman101 <keegan.thomas@gmail.com>
2019-05-23 11:18:37 +0000

    Modified templates to display posts from database.

---
+++ blog/.~c9_invoke_IAKqTo.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+
+
+
+
+
+
+
+
+
